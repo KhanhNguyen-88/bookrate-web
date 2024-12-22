14caf0faf0a786c73c44450d28a1d96f34b413a0
KhanhNguyen-88 <nguyenkhanhndtt@gmail.com>
2024-12-22 10:32:25 +0700

    fix percent

--- src/pages/BookDetail/components/RateChartItem/index.js
+++ src/pages/BookDetail/components/RateChartItem/index.js
@@ -2,18 +2,23 @@ import classNames from "classnames/bind";
 import style from "./RateChartItem.module.scss";
 
 const cx = classNames.bind(style);
-function RateChartItem({stars, ratings, percent}) {
+
+function RateChartItem({ stars, ratings, percent }) {
+  const formattedPercent = parseFloat(percent).toFixed(2); // Giới hạn 2 chữ số thập phân
+
   return (
     <div className={cx("wrapper")}>
-      <div className = {cx("title")}>
+      <div className={cx("title")}>
         <strong>{stars} stars</strong>
       </div>
-      <div className = {cx("main")}>
-        <div className = {cx("mainWrapper")}>
-          <div style={{width: `${percent}%`}}></div>
+      <div className={cx("main")}>
+        <div className={cx("mainWrapper")}>
+          <div style={{ width: `${percent}%` }}></div>
         </div>
       </div>
-      <div className = {cx("number")}>{ratings} ({percent}%)</div>
+      <div className={cx("number")}>
+        {ratings} ({formattedPercent}%)
+      </div>
     </div>
   );
 }
